ac0a0f9271b379716488c3981ae297295f971b1a
Ondrej Barci <ondrej.barci@loveit.sk>
2019-09-29 21:15:16 +0200

    first commit

--- src/main/java/com/obit/springdemo/MessageController.java
+++ src/main/java/com/obit/springdemo/MessageController.java
@@ -1,13 +1,17 @@
 package com.obit.springdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
+@Slf4j
 public class MessageController {
 
     private final MessageRepository messageRepository;
@@ -20,7 +24,16 @@ public class MessageController {
 
     @PostMapping("messages")
     public ResponseEntity<Message> createMessage(@RequestBody Message message){
-        Message save = messageRepository.save(message);
-        return new ResponseEntity<>(save, HttpStatus.CREATED);
+        messageRepository.save(message);
+        log.info("New message {} created", message);
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/messages")
+    public ResponseEntity<Void> deleteMessage(@RequestParam Long messageId){
+        messageRepository.deleteById(messageId);
+        log.info("Message with messageId {} deleted" , messageId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
